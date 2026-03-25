c8feb171616fa40fef1f35cc019320e482518637
TamaringoExpress <victor.casas@tecsup.edu.pe>
2026-03-25 14:30:15 -0500

    Initial commit

---
+++ Assets/code/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameMaNAGER : MonoBehaviour
+{
+    [SerializeField]private int Heal = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# tamaringo _2
+
