d1f7383fca8423a2c55b11889c98dcb7c5b91b32
980680035v-debug <victor.casas@tecsup.edu.pe>
2026-04-27 18:41:12 -0500

    asd

--- Assets/code/Bullet.cs
+++ Assets/code/Bullet.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class BULLET : MonoBehaviour
 {
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float velocity = 20f;
+    private Rigidbody rb;
    
     void Start()
     {
-        Debug.Log("XD");
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = transform.forward * velocity;
+        }
     }
-
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
